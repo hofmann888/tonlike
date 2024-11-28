9f1410cbeaadfba3faa2e93a419c03997a90cdb4
mhf <max.hofmann125x@gmail.com>
2024-11-28 10:55:45 +0300

    user context provider

--- src/app/components/header/header-balance.tsx
+++ src/app/components/header/header-balance.tsx
@@ -1,14 +1,25 @@
 'use client'
 
 import { useConnectedUser } from "@/app/hooks/useConnectedUser";
+import { UserContext } from "../providers/user-provider";
+import { useContext } from "react";
+import { User } from "@/app/lib/definitions";
 
 export default function HeaderBalance() {
-  const { balance, reward } = useConnectedUser();
+  const { user } = useContext(UserContext);
+
+  // const user = useContext(UserContext) as User;
+
+  // function test() {
+  //   console.log(user);
+  //   user.balance = user.balance + 1;
+  //   // user.update({balance: user.balance + 1});
+  // }
 
   return (
     <div className="header-balance">
-      <p>Balance: {balance}</p>
-      <p>Reward: {reward}</p>
+      <p>Balance: {user.balance}</p>
+      <p>Reward: {user.reward}</p>
     </div>
   );
 }
