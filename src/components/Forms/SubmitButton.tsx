458277b9226c269ba1274e9f3b8e983461bfb7a6
mhf <max.hofmann125x@gmail.com>
2024-12-27 14:10:30 +0300

    SubmitButton disabled prop

--- src/components/Forms/SubmitButton.tsx
+++ src/components/Forms/SubmitButton.tsx
@@ -1,14 +1,14 @@
 import { useFormStatus } from 'react-dom';
 import clsx from "clsx";
 
-export default function SubmitButton({ text }: { text: string }) {
+export default function SubmitButton({ text, disabled = false }: { text: string, disabled?: boolean }) {
   const { pending } = useFormStatus();
 
   return (
     <button 
       type="submit" 
-      className={clsx('submit-btn', {'disabled': pending})} 
-      disabled={pending} 
+      className={clsx('submit-btn', {'disabled': disabled || pending})} 
+      disabled={disabled || pending} 
       aria-disabled={pending}
     >
       {text}
