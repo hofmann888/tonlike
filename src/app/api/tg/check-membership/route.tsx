b20199f23ad629c3869133298cc31d26da1e492a
mhf <max.hofmann125x@gmail.com>
2025-01-08 11:51:53 +0300

    tg check membership api route

---
+++ src/app/api/tg/check-membership/route.tsx
@@ -0,0 +1,49 @@
+'use server'
+
+export async function POST(req: Request) {
+  try {
+    console.log('post check-membership');
+    const token = process.env.TWA_API_TOKEN;
+    if (!token) {
+      throw new Error('Telegram bot token is missing.');
+    }
+
+    const { tgId, channel } = await req.json();
+    console.log('tgId:', tgId);
+    console.log('channel:', channel);
+
+    if (!tgId || !channel) {
+      throw new Error('Invalid request: missing telegram user ig or channel name.');
+    }
+
+    let chat_id = channel;
+    if (!channel.startsWith('@') && !channel.startsWith('-100')) {
+      chat_id = '@' + channel;
+    }
+
+    const url = `https://api.telegram.org/bot${token}/getChatMember?chat_id=${encodeURIComponent(chat_id)}&user_id=${tgId}`;
+
+    const response = await fetch(url);
+    console.log('response:', response);
+    if (!response.ok) {
+      throw new Error(`Telegram API error: ${response.text()} - ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log('data:', data);
+    if (!data.ok) {
+      throw new Error(`Telegram API data error: ${JSON.stringify(data)}`);
+    }
+
+    const isMember = ['creator', 'administrator', 'member'].includes(data.result.status);
+    return Response.json({ success: true, result: isMember });
+  } catch (error: any) {
+    console.log(error);
+
+    let status = 500;
+    // if (error?.type === 'ERR_EXPIRED') { // TODO: error types?
+    //   status = 401; // Invalid credentials
+    // }
+    return Response.json({ success: false, error }, { status: status });
+  }
+}
