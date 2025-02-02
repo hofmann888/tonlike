cfeab133215f41abc92b9f238e528eb309b05978
hofmann888 <max.hofmann888@gmail.com>
2025-02-02 17:45:28 +0300

    app env console log

--- src/components/TWA/TWARoot.tsx
+++ src/components/TWA/TWARoot.tsx
@@ -20,6 +20,7 @@ import {
 const manifestUrl = 'https://maxhofm.github.io/stepik-5-5/tonconnect-manifest.json';
 
 function RootInner({ children }: PropsWithChildren) {
+  console.log('process.env.NEXT_PUBLIC_APP_ENV', process.env.NEXT_PUBLIC_APP_ENV);
   const isDev = process.env.NEXT_PUBLIC_APP_ENV === 'development';
 
   // Mock Telegram environment in development mode if needed.
