89537899594b58f427066303b0128e979738e36f
mhf <max.hofmann125x@gmail.com>
2025-02-02 14:37:19 +0300

    test commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,3 +7,4 @@ export async function middleware(request: NextRequest) { // TODO: do i need this
   // console.log('middleware');
   // return await updateSession(request); // TODO: caches and do not update session expire time
 }
+// test commit
