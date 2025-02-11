e78db6850d78aeb082d0d221d4f5bf3ca5241981
hofmann888 <max.hofmann888@gmail.com>
2025-02-11 22:16:59 +0300

    remove test error throw

--- src/app/auth/session.tsx
+++ src/app/auth/session.tsx
@@ -31,8 +31,6 @@ export async function setSession(user: User) {
   console.log('setSession');
   console.log(expires);
 
-  throw new Error('asdkjakshdkjas');
-
   cookies().set({ 
     name: 'session',
     value: await encrypt({ user, expDate: new Date(expires) }),
