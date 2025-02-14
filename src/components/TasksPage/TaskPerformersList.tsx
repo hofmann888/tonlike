ffadb62148babd4ba0e19dc3ec44cb314d4524c5
hofmann888 <max.hofmann888@gmail.com>
2025-02-14 10:46:31 +0300

    performers list todo comment

--- src/components/TasksPage/TaskPerformersList.tsx
+++ src/components/TasksPage/TaskPerformersList.tsx
@@ -9,9 +9,10 @@ import { useState } from "react";
 import PerformerBlockModal from "./PerformerBlockModal";
 import clsx from "clsx";
 
-// TODO?: move modal to tasklist component?
 // TODO: move card to separate component and add loaders on btn clicks
 // TODO: show no performers message on empty list
+// TODO?: move modal to tasklist component?
+// TODO?: show profit?
 export default function TaskPerformersList({ performers }: { performers: Performer[] }) {
   // console.log('TaskPerformersList:', performers);
   // for (let i = 0; i < 20; i++) {
