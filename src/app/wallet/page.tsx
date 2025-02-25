e4e90f0eef34d8a73df21383dbdb5b52e092d675
hofmann888 <max.hofmann888@gmail.com>
2025-02-25 10:11:04 +0300

    wallet page coming soon fix

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,44 +1,81 @@
 'use client'
 
-import { Tabs, Tab } from "@heroui/tabs";
-import { useTonConnect } from "@/hooks/useTonConnect";
-import DepositForm from "@/components/WalletPage/DepositForm";
-import WithdrawForm from "@/components/WalletPage/WithdrawForm";
-import WalletConnect from "@/components/WalletPage/WalletConnect";
+// import { Tabs, Tab } from "@heroui/tabs";
+// import { useTonConnect } from "@/hooks/useTonConnect";
+// import DepositForm from "@/components/WalletPage/DepositForm";
+// import WithdrawForm from "@/components/WalletPage/WithdrawForm";
+// import WalletConnect from "@/components/WalletPage/WalletConnect";
+import { openTelegramLink } from '@telegram-apps/sdk-react';
+import { Button } from "@heroui/button";
+import Image from 'next/image';
 
 export default function Walletage() {
-  const { connected } = useTonConnect();
+  // const { connected } = useTonConnect();
 
-  if (!connected) return (
-    <div className="wallet-page">
-      <WalletConnect />
-    </div>
-  );
+  const link = 'https://t.me/deadgens';
+
+  function btnClick() { // TODO: move to separate component (e.g. TgLink)
+    if (openTelegramLink.isAvailable()) {
+      openTelegramLink(link);
+    } 
+    // else if (openLink.isAvailable()) { // TODO!
+    //   openLink('https://telegram.org', {
+    //     tryInstantView: true,
+    //   });
+    // }
+  }
 
   return (
-    <div className="wallet-page">
-      <Tabs
-        color="primary"
-        variant="underlined"
-        radius="none"
-        classNames={{
-          base: "w-full mb-3",
-          cursor: "w-full",
-          tabList: "w-full p-0 border-b border-divider",
-          tab: "px-0 h-14 data-[selected=true]:bg-[#ffffff12]",
-          tabContent: "group-data-[selected=true]:text-foreground-900"
-          // cursor: "w-full bg-[#22d3ee]",
-          // tabContent: "group-data-[selected=true]:text-[#06b6d4]",
-        }}
-      >
-        <Tab key="deposit" title="Deposit" className="px-2">
-          <DepositForm />
-        </Tab>
+    <div className="py-5 px-2 h-full">
+      <div className="flex flex-col justify-center items-center h-full">
+        <p className="text-center text-4xl font-black text-primary-500">COMING SOON</p>
+        <p className="text-center text-large font-bold mt-3">STAY TUNED...</p>
+      </div>
 
-        <Tab key="withdraw" title="Withdraw" className="px-2">
-          <WithdrawForm />
-        </Tab>
-      </Tabs>
+      <div className="sticky bottom-28 w-full text-center">
+        <Button 
+          size="lg"
+          variant="light" 
+          aria-label="Telegram"
+          startContent={<Image width={30} height={30} src="/img/social/telegram.png" alt="telegram" />}
+          onPress={() => btnClick()}
+        >
+          Subscribe
+        </Button>
+      </div>
     </div>
   )
+
+  // if (!connected) return (
+  //   <div className="wallet-page">
+  //     <WalletConnect />
+  //   </div>
+  // );
+
+  // return (
+  //   <div className="wallet-page">
+  //     <Tabs
+  //       color="primary"
+  //       variant="underlined"
+  //       radius="none"
+  //       classNames={{
+  //         base: "w-full mb-3",
+  //         cursor: "w-full",
+  //         tabList: "w-full p-0 border-b border-divider",
+  //         tab: "px-0 h-14 data-[selected=true]:bg-[#ffffff12]",
+  //         tabContent: "group-data-[selected=true]:text-foreground-900"
+  //         // cursor: "w-full bg-[#22d3ee]",
+  //         // tabContent: "group-data-[selected=true]:text-[#06b6d4]",
+  //       }}
+  //     >
+  //       <Tab key="deposit" title="Deposit" className="px-2">
+  //         <DepositForm />
+  //       </Tab>
+
+  //       <Tab key="withdraw" title="Withdraw" className="px-2">
+  //         <WithdrawForm />
+  //       </Tab>
+  //     </Tabs>
+  //   </div>
+  // )
 }
