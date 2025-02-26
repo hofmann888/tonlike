ac7226b3a1603f3a27cbc267c7d52998819e4770
hofmann888 <max.hofmann888@gmail.com>
2025-02-26 14:53:25 +0300

    wallet components comment

--- src/components/WalletPage/WalletConnect.tsx
+++ src/components/WalletPage/WalletConnect.tsx
@@ -1,23 +1,23 @@
-'use client'
+// 'use client'
 
-import { TonConnectButton } from "@tonconnect/ui-react";
-import { useTonConnect } from "@/hooks/useTonConnect";
-import { CHAIN } from "@tonconnect/ui-react";
+// import { TonConnectButton } from "@tonconnect/ui-react";
+// import { useTonConnect } from "@/hooks/useTonConnect";
+// import { CHAIN } from "@tonconnect/ui-react";
 
-export default function WalletConnect() {
-  const { network } = useTonConnect();
+// export default function WalletConnect() {
+//   const { network } = useTonConnect();
   
-  return (
-    <div className="flex items-center justify-end my-5">
-      <div>
-      {network
-        ? network === CHAIN.MAINNET
-          ? "mainnet"
-          : "testnet"
-        : ""}
-      </div>
+//   return (
+//     <div className="flex items-center justify-end my-5">
+//       <div>
+//       {network
+//         ? network === CHAIN.MAINNET
+//           ? "mainnet"
+//           : "testnet"
+//         : ""}
+//       </div>
 
-      <TonConnectButton />
-    </div>
-  )
-}
+//       <TonConnectButton />
+//     </div>
+//   )
+// }
--- src/components/WalletPage/WithdrawForm.tsx
+++ src/components/WalletPage/WithdrawForm.tsx
@@ -1,45 +1,45 @@
-import { Form } from "@heroui/form";
-import { Input } from "@heroui/input";
-import { WithdrawFormState } from "@/lib/definitions";
-import { WithdrawFormSubmit } from "@/core/actions";
-import { useTonConnect } from "@/hooks/useTonConnect";
-import { useFormState } from 'react-dom';
-import SubmitButton from "@/components/Forms/SubmitButton";
+// import { Form } from "@heroui/form";
+// import { Input } from "@heroui/input";
+// import { WithdrawFormState } from "@/lib/definitions";
+// import { WithdrawFormSubmit } from "@/core/actions";
+// import { useTonConnect } from "@/hooks/useTonConnect";
+// import { useFormState } from 'react-dom';
+// import SubmitButton from "@/components/Forms/SubmitButton";
 
-export default function WithdrawForm() {
-  const min = 1;
-  const { address } = useTonConnect();
-  const initialState: WithdrawFormState = { errors: {}, message: null };
-  const [state, formAction] = useFormState(WithdrawFormSubmit, initialState);
+// export default function WithdrawForm() {
+//   const min = 1;
+//   const { address } = useTonConnect();
+//   const initialState: WithdrawFormState = { errors: {}, message: null };
+//   const [state, formAction] = useFormState(WithdrawFormSubmit, initialState);
   
-  return (
-    <Form action={formAction} className="withdraw-form" validationErrors={state?.errors}>
-      <Input
-        name="amount"
-        label="Amount"
-        type="number"
-        placeholder="0.00"
-        min={min}
-        step={0.1}
-        variant="bordered"
-        startContent={
-          <div className="pointer-events-none flex items-center">
-            <span className="text-default-400 text-small">$</span>
-          </div>
-        }
-      />
+//   return (
+//     <Form action={formAction} className="withdraw-form" validationErrors={state?.errors}>
+//       <Input
+//         name="amount"
+//         label="Amount"
+//         type="number"
+//         placeholder="0.00"
+//         min={min}
+//         step={0.1}
+//         variant="bordered"
+//         startContent={
+//           <div className="pointer-events-none flex items-center">
+//             <span className="text-default-400 text-small">$</span>
+//           </div>
+//         }
+//       />
 
-      <Input name="address" label="Address" variant="bordered" defaultValue={address ?? ''} isClearable />
+//       <Input name="address" label="Address" variant="bordered" defaultValue={address ?? ''} isClearable />
 
-      <div id="fields-error" aria-live="polite" aria-atomic="true">
-        {state?.message &&
-          <p className="mt-2 text-sm text-red-500" key={state.message}>
-            {state.message}
-          </p>
-        }
-      </div>
+//       <div id="fields-error" aria-live="polite" aria-atomic="true">
+//         {state?.message &&
+//           <p className="mt-2 text-sm text-red-500" key={state.message}>
+//             {state.message}
+//           </p>
+//         }
+//       </div>
 
-      <SubmitButton content="Withdraw" />
-    </Form>
-  )
-}
+//       <SubmitButton content="Withdraw" />
+//     </Form>
+//   )
+// }
