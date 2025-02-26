2c1aae77a124fcb059d6acf69c8edca0fb7468c5
hofmann888 <max.hofmann888@gmail.com>
2025-02-26 16:08:11 +0300

    test error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,8 @@ export default async function RootLayout({
 
   const defaultTheme = 'dark';
 
+  // throw new Error('layout error');
+
   return (
     <html lang="en" className={defaultTheme}>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
