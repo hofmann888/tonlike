c1d187faa2247977d2229a87f5fbc0083cb9caf8
hofmann888 <max.hofmann888@gmail.com>
2025-02-28 09:25:06 +0300

    home tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,27 @@
 
 import { tgCheckMembershipRequest, tgCheckBoostRequest } from "@/utils/requests";
 import { deleteSession } from "./auth/session";
-import { tgOpenLink } from "@/utils/helpers";
+import { tgOpenLink, formatLink } from "@/utils/helpers";
 import { Button } from "@heroui/button";
+import { ServiceNameEnum } from "@/lib/definitions";
 
 
 export default function Home() {
   const test = async () => {
     // await tgCheckMembershipRequest(6681557705, '@test_channel_chat00800');
     // await tgCheckBoostRequest(5229340312, '@tonlike_app');
-    tgOpenLink('https://t.me/deadgens');
+    // tgOpenLink('https://t.me/deadgens');
+
+    // let link = '@tonlike_app';
+    // let link = 'https://t.me/tonlike_app/';
+    // let link = '@daikiserials';
+    // let link = 'http://tiktok.com/@daikiserials/';
+    let link = 'http://www.twitter.com/tonlike/';
+    // let link = '@tonlike';
+
+    console.log('link before', link);
+    link = formatLink(link, ServiceNameEnum.X, 'link');
+    console.log('link after', link);
   }
 
   return (
