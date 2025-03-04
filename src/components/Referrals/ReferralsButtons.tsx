1db597ed313bb501a70a0fc29ab4e2e7bb26c727
hofmann888 <max.hofmann888@gmail.com>
2025-03-04 22:30:41 +0300

    referral message fix

--- src/components/Referrals/ReferralsButtons.tsx
+++ src/components/Referrals/ReferralsButtons.tsx
@@ -6,18 +6,15 @@ import { FaCopy } from "react-icons/fa";
 import { useUser } from "@/hooks/useUser";
 import { shareURL } from '@telegram-apps/sdk-react';
 
+// window.clipboardData.setData("Text", 'Copy this text to clipboard') // TODO: support for old browsers
+// TODO?: NotAllowedError: Failed to execute 'writeText on 'Clipboard': Document is not focused. 
+//        mobile on copy btn click in the first time (link is copying though)
+// TODO?: add input with link (to copy not through button)?
 export default function ReferralsButtons() {
   const { tgId } = useUser();
   const bot = process.env.NEXT_PUBLIC_TG_BOT_NAME as string;
   const app = process.env.NEXT_PUBLIC_TG_APP_NAME as string;
   const link = `https://t.me/${bot}/${app}?startapp=${tgId}`;
-
-  // window.clipboardData.setData("Text", 'Copy this text to clipboard') // TODO: support for old browsers
-
-  // TODO?: NotAllowedError: Failed to execute 'writeText on 'Clipboard': Document is not focused. 
-  //       mobile on copy btn click in the first time (link is copying though)
-
-  // TODO?: add input with link (to copy not through button)?
 
   return (
     <div className="w-full flex sticky bottom-[60px] py-2 bg-background">
