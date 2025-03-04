4f392464494f47dc5aa92a2ed650eddaede8beb0
hofmann888 <max.hofmann888@gmail.com>
2025-03-04 17:15:30 +0300

    telegraf + tg webhook fix

--- src/app/api/tg/webhook/route.ts
+++ src/app/api/tg/webhook/route.ts
@@ -4,16 +4,19 @@ import { headers } from 'next/headers';
 
 export async function POST(req: Request) {
   try {
-    // const body = await req.json();
     const headersList = headers();
-    const token = process.env.TG_BOT_SECRET_TOKEN;
+    const token = process.env.TG_BOT_WEBHOOK_TOKEN;
     const secret = headersList.get('X-Telegram-Bot-Api-Secret-Token');
+    console.log('secret:', secret);
 
     if (!secret || !token || secret !== token) {
       throw new Error('Webhook auth failed.');
     }
 
-    Response.json(null);
+    const body = await req.json();
+    console.log('body:', body);
+
+    Response.json({"message": "ok"}); // Response.json(null);
   } catch (error: any) {
     console.log('Webhook Error:', error);
     return Response.json({ success: false, error }, { status: 500 });
