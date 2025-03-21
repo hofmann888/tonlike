94463853b273c283e69da2455d07914c208cd8a2
hofmann888 <max.hofmann888@gmail.com>
2025-03-21 09:02:14 +0300

    earn quest list warning

--- src/components/Earn/EarnQuestList.tsx
+++ src/components/Earn/EarnQuestList.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Chip } from "@heroui/chip";
+import { Alert } from "@heroui/alert";
 import { Quest } from "@/lib/definitions";
 import { useTranslations } from "next-intl";
 import EarnQuestCard from "./EarnQuestCard";
@@ -10,6 +11,12 @@ export default function EarnQuestList({ quests }: { quests: Quest[] }) {
 
   return (
     <div className="px-2">
+      <Alert 
+        color="warning"
+        className="px-3 gap-0"
+        title={<p>{t('warning')}</p>} 
+      />
+
       <Chip color="primary" variant="dot" className="mb-2">{t('daily')}</Chip>
       {quests.map((quest) => (
         quest.daily && 
