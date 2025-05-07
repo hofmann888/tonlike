4940e61800338acc250861c5f4354b89670a7ace
hofmann888 <max.hofmann888@gmail.com>
2025-05-07 11:01:10 +0300

    footer links prefetch

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -35,9 +35,9 @@ export default function Footer() {
         classNames={{ 
           base: "w-full h-full", 
           tabList: "w-full h-full relative rounded-none p-0 border-b border-divider gap-6 max-[480px]:gap-3 max-[410px]:gap-0",
-          tab: "h-full w-1/5",
+          tab: "h-full w-1/5 p-0 ",
           cursor: "w-full",
-          tabContent: "group-data-[selected=true]:text-foreground-900"
+          tabContent: "group-data-[selected=true]:text-foreground-900 w-full h-full"
           // cursor: "w-full bg-[#22d3ee]",
         }}
       >
@@ -45,14 +45,12 @@ export default function Footer() {
           const LinkIcon = link.icon;
           return (
             <Tab 
-              as={Link}
-              key={link.href} 
-              href={link.href}
+              key={link.href}
               title={
-                <div className="flex flex-col items-center justify-center">
+                <Link href={link.href} prefetch={true} className="w-full h-full flex flex-col items-center justify-center">
                   <LinkIcon className="w-6 h-6 mt-1" />
                   <span className="text-xs mt-1 max-[370px]:text-[0.65rem]">{t(`nav.${link.title}`)}</span>
-                </div>
+                </Link>
               }
             />
           );
