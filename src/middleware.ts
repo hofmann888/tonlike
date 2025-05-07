6227207fbe82c57f6dfc4b33cf6fd09ebce78752
hofmann888 <max.hofmann888@gmail.com>
2025-05-07 17:40:50 +0300

    earn redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,6 @@ import { AppEnvEnum } from "./lib/definitions";
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   if (pathname === '/' && process.env.NEXT_PUBLIC_APP_ENV === AppEnvEnum.PROD) {
-    return NextResponse.redirect(new URL('/earn', request.url));
+    return NextResponse.redirect(new URL('/earn/tasks', request.url));
   }
 }
